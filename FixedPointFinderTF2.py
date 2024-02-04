093ae552a69c4b92cd9a59279ffb1f0c2166fb3f
Eduardo Sanchez Karhunen <esanchek@gmail.com>
2024-02-04 18:20:48 +0100

    feat: pure tf2 fpf implementation

--- FixedPointFinderTF2.py
+++ FixedPointFinderTF2.py
@@ -118,7 +118,7 @@ class FixedPointFinderTF2(FixedPointFinderBase):
             iter_count = 1
             t_start = time.time()
 
-            print(inputs_bxd.device, x_bxd.device, optimizer.device, q_prev_b.device)
+            print(inputs_bxd.device, x_bxd.device, q_prev_b.device)
 
             while True:
                 with tf.GradientTape() as tape:
@@ -129,6 +129,8 @@ class FixedPointFinderTF2(FixedPointFinderBase):
                     q_b = 0.5 * tf.reduce_sum(tf.square(dx_bxd), axis=1)
                     # mean for all batch
                     q_scalar = tf.math.reduce_mean(q_b)
+
+                    print(F_x_bxd.device, dx_bxd.device, q_b.device, q_scalar.device)
 
                 # optimizer step vs joint q: q_scalar
                 grads = tape.gradient(q_scalar, [x_bxd])
